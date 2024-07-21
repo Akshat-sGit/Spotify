bd943ada5f4364b956858e25f5682cd14caa842c
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-07-21 09:10:33 +0530

    app theme setup created

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart'; 
+import 'package:spotify/core/configs/theme/app_colors.dart'; 
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme{
+  
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary, 
+    brightness: Brightness.light, 
+    scaffoldBackgroundColor: AppColors.lightBackgorund, 
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary, 
+        textStyle: GoogleFonts.plusJakartaSans(
+          fontSize: 20, 
+          fontWeight: FontWeight.bold, 
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    ), 
+  ); 
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary, 
+    brightness: Brightness.dark, 
+    scaffoldBackgroundColor: AppColors.darkBackground, 
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary, 
+        textStyle: GoogleFonts.plusJakartaSans(
+          fontSize: 20, 
+          fontWeight: FontWeight.bold, 
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    ), 
+  ); 
+
+} 
